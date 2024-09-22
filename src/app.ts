c75b42ad27372e6739898df2cacd181dbc9a7e57
tanzilmia <ziltan590@gmail.com>
2024-09-22 08:18:33 +0600

    add basic setup

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import { StudentRoutes } from "./app/modules/student/student.route";
+const app: Application = express();
+
+// parser
+app.use(express.json());
+app.use(cors());
+
+// application router
+
+app.use("/api/v1/students", StudentRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World! Update the code");
+});
+
+export default app;
